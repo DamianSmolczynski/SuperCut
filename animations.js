76afe27892a27ef3dbe6a3f86d1a99008ba202f1
Damian Smolczyński <79711730+DamianSmolczynski@users.noreply.github.com>
2021-04-09 17:19:56 +0200

    index, style and animation
    I decided to modify nav-bar animations using css "keyframes" future and two jQuery methods; .on(0 ; .css(). It's desktop browser only. To be honest, I feel like jQuery is not optimal option for 2021, so in next improvements I'll try to use vanilla JS.

--- animations.js
+++ animations.js
@@ -2,10 +2,12 @@ let menuButton = $(".menu");
 let contactButton = $("address a");
 menuButton.on("mouseover", function(){
     $(this).css({
-            'font-size' : '3.5vw'}
+            "animation-name" : "navButtons", "animation-duration": "0.5s", "animation-fill-mode": "forwards",
+            "animation-timing-function": "ease" }
     );});
 menuButton.on("mouseout", function(){
-    $(this).css("font-size","3.1vw");});
+    $(this).css({
+        "animation-name" : "navButtonsReturn", "animation-duration": "0.5s", "animation-timing-function": "ease" });});
 
 contactButton.on("mouseover", function(){
     $(this).css({
